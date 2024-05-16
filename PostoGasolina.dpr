ff95db0063620a8064fb382112eb28fbb6e55d7d
oLucasPopov <lucaspopov@protonmail.com>
2024-05-15 21:06:24 -0300

    feat: atualiza units do projeto

--- PostoGasolina.dpr
+++ PostoGasolina.dpr
@@ -2,7 +2,16 @@ program PostoGasolina;
 
 uses
   Vcl.Forms,
-  untPrincipal in 'untPrincipal.pas' {frmPrincipal};
+  untPrincipal in 'untPrincipal.pas' {frmPrincipal},
+  untCadAbastecimento in 'UI\untCadAbastecimento.pas' {frmCadAbastecimento},
+  untDmConnection in 'DataModules\untDmConnection.pas' {dmConnection: TDataModule},
+  untControllerNovoAbastecimento in 'Controllers\untControllerNovoAbastecimento.pas',
+  untModelsAbastecimento in 'Models\untModelsAbastecimento.pas',
+  untRepositoriosNovoAbastecimento in 'Repositories\untRepositoriosNovoAbastecimento.pas',
+  untHelpersConversao in 'Helpers\untHelpersConversao.pas',
+  untHelpersDB in 'Helpers\untHelpersDB.pas',
+  untHelpersFormatacao in 'Helpers\untHelpersFormatacao.pas',
+  untHelpersMensagem in 'Helpers\untHelpersMensagem.pas';
 
 {$R *.res}
 
@@ -10,5 +19,6 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TfrmPrincipal, frmPrincipal);
+  Application.CreateForm(TdmConnection, dmConnection);
   Application.Run;
 end.
