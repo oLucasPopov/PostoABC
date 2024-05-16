39c18f3d6c8df5425dcbf6f63f9c4fedbbcf7736
oLucasPopov <lucaspopov@protonmail.com>
2024-05-15 21:05:41 -0300

    feat: adiciona form para cadastro de abastecimento

---
+++ UI/untCadAbastecimento.dfm
@@ -0,0 +1,139 @@
+object frmCadAbastecimento: TfrmCadAbastecimento
+  Left = 0
+  Top = 0
+  Caption = 'Cadastro de Abastecimentos'
+  ClientHeight = 85
+  ClientWidth = 504
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poOwnerFormCenter
+  OnCloseQuery = FormCloseQuery
+  OnCreate = FormCreate
+  DesignSize = (
+    504
+    85)
+  PixelsPerInch = 96
+  TextHeight = 13
+  object lblBomba: TLabel
+    Left = 8
+    Top = 8
+    Width = 32
+    Height = 13
+    Caption = 'Bomba'
+  end
+  object lblPrecoCombustivel: TLabel
+    Left = 240
+    Top = 8
+    Width = 27
+    Height = 13
+    Caption = 'Pre'#231'o'
+  end
+  object Label1: TLabel
+    Left = 152
+    Top = 8
+    Width = 26
+    Height = 13
+    Caption = 'Litros'
+  end
+  object lblTotal: TLabel
+    Left = 384
+    Top = 8
+    Width = 24
+    Height = 13
+    Caption = 'Total'
+  end
+  object edtPreco: TDBEdit
+    Left = 240
+    Top = 24
+    Width = 137
+    Height = 21
+    TabStop = False
+    Color = clInactiveCaption
+    DataField = 'preco_litro'
+    DataSource = dsBomba
+    ReadOnly = True
+    TabOrder = 2
+    OnChange = edtPrecoChange
+  end
+  object btnConfirmar: TButton
+    Left = 425
+    Top = 54
+    Width = 75
+    Height = 25
+    Anchors = [akRight, akBottom]
+    Caption = 'Confirmar'
+    TabOrder = 4
+    OnClick = btnConfirmarClick
+    ExplicitTop = 64
+  end
+  object btnCancelar: TButton
+    Left = 337
+    Top = 54
+    Width = 75
+    Height = 25
+    Anchors = [akRight, akBottom]
+    Caption = 'Cancelar'
+    TabOrder = 5
+    OnClick = btnCancelarClick
+    ExplicitTop = 64
+  end
+  object cmbBomba: TDBLookupComboBox
+    Left = 8
+    Top = 24
+    Width = 137
+    Height = 21
+    KeyField = 'id'
+    ListField = 'descricao_bomba'
+    ListSource = dsBomba
+    TabOrder = 0
+  end
+  object edtTotal: TEdit
+    Left = 384
+    Top = 24
+    Width = 113
+    Height = 21
+    TabStop = False
+    Color = clInactiveCaption
+    ReadOnly = True
+    TabOrder = 3
+  end
+  object edtLitros: TEdit
+    Left = 152
+    Top = 24
+    Width = 81
+    Height = 21
+    TabOrder = 1
+    Text = '0,000'
+    OnChange = edtLitrosChange
+  end
+  object dsBomba: TDataSource
+    DataSet = qryBomba
+    Left = 48
+    Top = 56
+  end
+  object qryBomba: TFDQuery
+    Connection = dmConnection.connection
+    SQL.Strings = (
+      'select bc.id, bc.DESCRICAO_BOMBA, tc.PRECO_LITRO '
+      'from BOMBAS_COMBUSTIVEL bc '
+      'inner join TANQUES_COMBUSTIVEL tc on tc.ID = bc.ID_TANQUE')
+    Left = 8
+    Top = 56
+    object qryBombaid: TIntegerField
+      FieldName = 'id'
+    end
+    object qryBombapreco_litro: TFMTBCDField
+      FieldName = 'preco_litro'
+      Size = 2
+    end
+    object qryBombadescricao_bomba: TStringField
+      FieldName = 'descricao_bomba'
+      Size = 256
+    end
+  end
+end
---
+++ UI/untCadAbastecimento.pas
@@ -0,0 +1,147 @@
+unit untCadAbastecimento;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.DBCtrls, Vcl.Mask,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param,
+  FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf,
+  FireDAC.Stan.Async, FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client, untDmConnection, Data.FMTBcd, Data.SqlExpr;
+
+type
+  TfrmCadAbastecimento = class(TForm)
+    lblBomba: TLabel;
+    edtPreco: TDBEdit;
+    lblPrecoCombustivel: TLabel;
+    Label1: TLabel;
+    lblTotal: TLabel;
+    btnConfirmar: TButton;
+    btnCancelar: TButton;
+    dsBomba: TDataSource;
+    qryBomba: TFDQuery;
+    qryBombaid: TIntegerField;
+    qryBombapreco_litro: TFMTBCDField;
+    qryBombadescricao_bomba: TStringField;
+    cmbBomba: TDBLookupComboBox;
+    edtTotal: TEdit;
+    edtLitros: TEdit;
+    procedure btnConfirmarClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure edtLitrosChange(Sender: TObject);
+    procedure edtPrecoChange(Sender: TObject);
+    procedure btnCancelarClick(Sender: TObject);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+  private
+    function SomenteNumero(str: string): string;
+    procedure Totalizar;
+    function ConfirmaCancelamento: Boolean;
+    procedure LimparCampos;
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmCadAbastecimento: TfrmCadAbastecimento;
+
+
+implementation
+
+uses untModelsAbastecimento, untHelpersMensagem, untHelpersFormatacao,
+untControllerNovoAbastecimento, untHelpersConversao;
+
+{$R *.dfm}
+
+function TfrmCadAbastecimento.SomenteNumero(str: string): string;
+var
+  x: integer;
+begin
+  Result := '';
+  for x := 0 to Length(str) - 1 do
+    if CharInSet(str.Chars[x], ['0' .. '9']) then
+      Result := Result + str.Chars[x];
+end;
+
+function TfrmCadAbastecimento.ConfirmaCancelamento: Boolean;
+begin
+  Result := TMensagemHelper.MensagemSimNao('Deseja realmente cancelar o cadastro?');
+end;
+
+procedure TfrmCadAbastecimento.btnCancelarClick(Sender: TObject);
+begin
+  Self.Close;
+end;
+
+procedure TfrmCadAbastecimento.edtLitrosChange(Sender: TObject);
+begin
+  edtLitros.Text := TFormatar.Litro(SomenteNumero(edtLitros.Text));
+  edtLitros.SelStart := Length(edtLitros.Text);
+
+  Totalizar;
+end;
+
+procedure TfrmCadAbastecimento.edtPrecoChange(Sender: TObject);
+begin
+  Totalizar;
+end;
+
+procedure TfrmCadAbastecimento.Totalizar;
+var
+  litros: Real;
+begin
+  litros := StrToFloat(edtLitros.Text);
+  edtTotal.Text := FormatFloat('#,##0.00', qryBomba.FieldByName('preco_litro').AsFloat * litros);
+end;
+
+procedure TfrmCadAbastecimento.LimparCampos;
+begin
+  cmbBomba.KeyValue := null;
+  edtLitros.Text    := '0';
+end;
+
+procedure TfrmCadAbastecimento.btnConfirmarClick(Sender: TObject);
+const
+  sucesso = 'Cadastro realizado com sucesso!'
++sLineBreak+'Deseja manter o cadastro aberto para cadastrar outro abastecimento?';
+var
+  NovoAbastecimento: TNovoAbastecimento;
+  ControllerNovoAbastecimento: TControllerNovoAbastecimento;
+begin
+  try
+    NovoAbastecimento.Gerar(cmbBomba.KeyValue.ToIntDef(-1)
+                           ,edtPreco.Text.toFloatLocale
+                           ,edtLitros.Text.toFloatLocale);
+
+    ControllerNovoAbastecimento.Cadastrar(NovoAbastecimento);
+
+    if not TMensagemHelper.MensagemSimNao(sucesso) then
+    begin
+      Self.OnCloseQuery := nil;
+      Self.Close;
+    end;
+
+
+    LimparCampos;
+    cmbBomba.SetFocus;
+  except
+    on E:Exception do
+    begin
+      TMensagemHelper.MensagemErro('Erro ao cadastrar abastecimento', E);
+    end;
+  end;
+end;
+
+procedure TfrmCadAbastecimento.FormCloseQuery(Sender: TObject;
+  var CanClose: Boolean);
+begin
+  CanClose := ConfirmaCancelamento;
+end;
+
+procedure TfrmCadAbastecimento.FormCreate(Sender: TObject);
+begin
+qryBomba.Active := True;
+end;
+
+end.
